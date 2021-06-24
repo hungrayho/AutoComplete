f618c3f11e4d217a1b3b037183d439705d41e30d
hungrayho <hunkinhr@gmail.com>
2021-06-24 14:24:44 +0800

    dataclean: added split by exc mark and question mark; added strip single words

--- data_clean.py
+++ data_clean.py
@@ -3,6 +3,8 @@ import pandas as pd
 import re
 import os
 
+from itertools import filterfalse
+
 # config
 SEP = os.sep
 DATA_DIR =  os.path.dirname(os.path.realpath(__file__)) + SEP + "data" + SEP
@@ -10,7 +12,7 @@ DATA_DIR =  os.path.dirname(os.path.realpath(__file__)) + SEP + "data" + SEP
 #%% ingest
 df = pd.read_json(DATA_DIR + "Video_Games_5.json", lines=True)
 
-#%% Cleaning
+#%% Cleaning json payload in pandas
 # drop unnecessary columns
 df = df[["reviewText"]]
 
@@ -20,47 +22,56 @@ df.replace(r'\n',' ', regex = True, inplace = True)
 # drop missing values
 df.dropna(axis = 0, inplace = True)
 
-# split sentences
+print("extracted data from json payload")
+#%% cleaning extracted text in python
+# split sentences around "."
 # df_clean["sentences"] = pd.concat([pd.Series([row['reviewText'].rsplit('.') for _, row in df.iterrows()])]).reset_index()
 split = [row['reviewText'].rsplit('.') for _, row in df.iterrows()]
 
-# flatten list
-split = [item for sublist in split for item in sublist]
+# split sentences around "!"
+# for sublist in split:
+#     for line in sublist:
+split = [line.rsplit('!') for sublist in split for line in sublist]
 
-# filter white spaces
+# split sentences around "?"
+# for sublist in split:
+    # for line in sublist:
+split = [line.rsplit('?') for sublist in split for line in sublist]
+
+# flatten list
+# for sublist in split:
+#     for line in sublist:
+split = [line for sublist in split for line in sublist]
+
+# filter empty strings
 split = list(filter(bool, split))
 
 # filter single characters
-from itertools import filterfalse
-
 split[:] = filterfalse(lambda x: len(x) == 1, split)
 
-# filter embedded links
-import re
+# filter single words
+regex = re.compile("^[A-Za-z]+$")
+split[:] = filterfalse(regex.match, split)
 
+# filter embedded links
 # for line in split:
 #     line = re.sub(r"(&nbsp)(.*)((<\/a>))", "", line)
-
 split = [re.sub(r"(&nbsp)(.*)((<\/a>))", "", line) for line in split]
-
 # to test:
 # list(set(split) - set(re.sub(r"(&nbsp)(.*)((<\/a>))", "", line) for line in split))
 
 # strip leading spaces
 split = [line.lstrip() for line in split]
 
-# filter emojis
+# filter emoticons
 split = [re.sub(r"( :\)| :D)", "", line) for line in split]
 
+# TODO: spell check
 
-# spell check
-# skip
-print("cleaning complete...")
+print("data cleaning complete...")
 #%%
 FILE_PATH = DATA_DIR + "dataset.txt"
 with open(FILE_PATH, 'w') as f:
     for i in split:
         f.write("%s\n" % i)
 print("processed data written to {}".format(FILE_PATH))
-
-# %%
