c16affbd9052d50368c400bb2be55dd2f728f9a5
hungrayho <hunkinhr@gmail.com>
2021-06-26 03:42:10 +0800

    main package skeleton

---
+++ main.py
@@ -0,0 +1,5 @@
+# imports
+
+if __name__ == "__main__":
+    # main loop
+    pass
---
+++ src/interface/metaclasses.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+
+class meta_inference_engine(ABC):
+    
+    @abstractmethod
+    def sentence_to_vector():
+        pass
+
+    @abstractmethod
+    def translate():
+        pass
+
+    @abstractmethod
+    def predict():
+        pass
+
+
+class meta_api(ABC):
+    
+    @abstractmethod
+    def autoc():
+        pass
+
+
+class meta_app(ABC):
+    
+    @abstractmethod
+    def app():
+        pass
+
+    @abstractmethod
+    def send():
+        pass
+
+    @abstractmethod
+    def listen():
+        pass
