c81ee7e281797c67ad82e97023d2f5f402afcb54
hungrayho <hunkinhr@gmail.com>
2021-06-25 23:46:22 +0800

    start reverse engineering loadpy

--- load.py
+++ load.py
@@ -44,3 +44,99 @@ with tf.keras.backend.get_session() as sess:
 
 
 print("loaded")
+
+#%% Create inference model
+# Create the encoder model from the tensors we previously declared.
+encoder_model = Model(encoder_inputs, [encoder_out, state_h, state_c])
+
+# Generate a new set of tensors for our new inference decoder. Note that we are using new tensors, 
+# this does not preclude using the same underlying layers that we trained on. (e.g. weights/biases).
+
+inf_decoder_inputs = Input(shape=(None,), name="inf_decoder_inputs")
+# We'll need to force feed the two state variables into the decoder each step.
+state_input_h = Input(shape=(units*2,), name="state_input_h")
+state_input_c = Input(shape=(units*2,), name="state_input_c")
+decoder_res, decoder_h, decoder_c = decoder_lstm(
+    decoder_emb(inf_decoder_inputs), 
+    initial_state=[state_input_h, state_input_c])
+inf_decoder_out = decoder_d2(decoder_d1(decoder_res))
+inf_model = Model(inputs=[inf_decoder_inputs, state_input_h, state_input_c], 
+                  outputs=[inf_decoder_out, decoder_h, decoder_c])
+
+#%% Model test inference functions
+# Converts the given sentence (just a string) into a vector of word IDs
+# Output is 1-D: [timesteps/words]
+
+def catch(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception as e:
+        pass
+
+def sentence_to_vector(sentence, lang):
+
+    pre = sentence
+    vec = np.zeros(len_input)
+    sentence_list = [catch(lambda: lang.word2idx[s]) for s in pre.split(' ')]
+    for i,w in enumerate(sentence_list):
+        vec[i] = w
+    return vec
+
+# Given an input string, an encoder model (infenc_model) and a decoder model (infmodel),
+def translate(input_sentence, infenc_model, infmodel):
+    sv = sentence_to_vector(input_sentence, input_lang)
+    sv = sv.reshape(1,len(sv))
+    [emb_out, sh, sc] = infenc_model.predict(x=sv)
+    
+    i = 0
+    start_vec = target_lang.word2idx["<start>"]
+    stop_vec = target_lang.word2idx["<end>"]
+    
+    cur_vec = np.zeros((1,1))
+    cur_vec[0,0] = start_vec
+    cur_word = "<start>"
+    output_sentence = ""
+
+    while cur_word != "<end>" and i < (len_target-1):
+        i += 1
+        if cur_word != "<start>":
+            output_sentence = output_sentence + " " + cur_word
+        x_in = [cur_vec, sh, sc]
+        [nvec, sh, sc] = infmodel.predict(x=x_in)
+        cur_vec[0,0] = np.argmax(nvec[0,0])
+        cur_word = target_lang.idx2word[np.argmax(nvec[0,0])]
+    return output_sentence
+
+print ("Model testing functions loaded...")
+#%% Test Inference
+#Note that only words that we've trained the model on will be available, otherwise you'll get an error.
+
+print ("Running test inference...")
+test = [
+    'hi there',
+    'hell',
+    'presentation please fin',
+    'resignation please find at',
+    'resignation please ',
+    'have a nice we',
+    'let me ',
+    'promotion congrats ',
+    'christmas Merry ',
+    'please rev',
+    'please ca',
+    'thanks fo',
+    'Let me kno',
+    'Let me know if y',
+    'this soun',
+    'is this call going t'
+]
+  
+
+import pandas as pd
+output = []  
+for t in test:  
+  output.append({"Input seq":t.lower(), "Pred. Seq":translate(t.lower(), encoder_model, inf_model)})
+
+results_df = pd.DataFrame.from_dict(output) 
+results_df.head(len(test))
+
