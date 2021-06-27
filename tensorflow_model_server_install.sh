2ac193d909077e74a6856956e4283aea5a3fed64
hungrayho <hunkinhr@gmail.com>
2021-06-28 02:02:19 +0800

    tf serve innstall script

--- tensorflow_model_server_install.sh
+++ tensorflow_model_server_install.sh
@@ -2,4 +2,5 @@ sudo apt install curl
 echo "deb [arch=amd64] http://storage.googleapis.com/tensorflow-serving-apt stable tensorflow-model-server tensorflow-model-server-universal" | sudo tee /etc/apt/sources.list.d/tensorflow-serving.list && curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install tensorflow-model-server
+sudo apt-get upgrade tensorflow-model-server
 tensorflow_model_server --version
